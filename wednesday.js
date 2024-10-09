abdc1442289e78fb390f2df1c55d1cddde96651f
Thounny <thounnykeo@gmail.com>
2024-10-09 10:11:38 -0400

    Expressions | Precedence

---
+++ wednesday.js
@@ -0,0 +1,17 @@
+// EXPRESSIONS (calculations with operators e.g. + - * / %)
+
+// PRECEDENCE (which thing happened in which order)
+// PEMDAS
+
+x = 3 * 2 + 9 * 10;
+
+xp = 3 * 2 + 9 * 10;
+
+p1 = 3 * 2;
+p2 = 9 * 10;
+x3 = p1 + p2;
+
+// OUTPUT = 90
+console.log(x3);
+
+// FOOD COST
