b51d7e3735ceddc72b3a71260359d45f5b7e2717
Thounny <thounnykeo@gmail.com>
2024-10-09 12:00:08 -0400

    Boolean | TRUE / FALSE

---
+++ Week3/conditionals.js
@@ -0,0 +1,14 @@
+// Computer values are 0 and 1, (OFF and ON)
+// Logic TRUE or FALSE, 0 is usually false and non-zero is usually true
+
+// Bool - Boolean (True/False) Data Type - Reason/Philosophy into math?
+
+hasLicense = false;
+
+console.log(hasLicense);
+// FALSE
+
+if (hasLicense) {
+  console.log("SKRRRRRRRRRRRRRRRR");
+}
+
