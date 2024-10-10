a4675459ebc509bb41355e9b73095661b0426003
Thounny <thounnykeo@gmail.com>
2024-10-10 11:43:38 -0400

    Function for Area

---
+++ Week3/area.js
@@ -0,0 +1,16 @@
+// area is the identifier/name
+// width and height are received parameters
+function area(width, height) {
+  return width * height;
+}
+
+// 10 and 20 are arguments being passed
+console.log(area(10, 20));
+console.log(area(12, 20));
+console.log(area(24, 40));
+
+// height = 10;
+// width = 20;
+// myArea = area(width, height);
+
+// console.log(myArea);
